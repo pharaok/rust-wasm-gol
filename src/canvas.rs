c20ab7ae07fd0e29d280398d314c8727a6262ae0
pharaok <farouk.tamer03@gmail.com>
2024-02-22 17:49:46 +0200

    create grid state

--- src/canvas.rs
+++ src/canvas.rs
@@ -1,5 +1,12 @@
-use leptos::*;
-use web_sys::{wasm_bindgen::JsCast, CanvasRenderingContext2d};
+use std::collections::HashMap;
+
+use leptos::{leptos_dom::logging::console_log, *};
+use web_sys::{
+    wasm_bindgen::{JsCast, JsValue},
+    CanvasRenderingContext2d,
+};
+
+const CELL_SIZE: f64 = 32.0;
 
 #[component]
 pub fn Canvas() -> impl IntoView {
@@ -8,6 +15,8 @@ pub fn Canvas() -> impl IntoView {
 
     let inner_width = window().inner_width().unwrap().as_f64().unwrap();
     let inner_height = window().inner_height().unwrap().as_f64().unwrap();
+
+    let (grid, set_grid) = create_signal(HashMap::<(i32, i32), u8>::new());
 
     create_effect(move |_| {
         canvas_ref().unwrap().set_width(inner_width as u32);
@@ -27,8 +36,36 @@ pub fn Canvas() -> impl IntoView {
 
     create_effect(move |_| {
         let ctx = context().unwrap();
-        ctx.fill_rect(0.0, 0.0, inner_width, inner_height)
+        ctx.set_fill_style(&JsValue::from_str("black"));
+        ctx.fill_rect(0.0, 0.0, inner_width, inner_height);
+
+        ctx.set_fill_style(&JsValue::from_str("white"));
+        for ((x, y), v) in grid() {
+            if v == 1 {
+                ctx.fill_rect(
+                    x as f64 * CELL_SIZE,
+                    y as f64 * CELL_SIZE,
+                    CELL_SIZE,
+                    CELL_SIZE,
+                );
+            }
+        }
     });
 
-    view! { <canvas _ref=canvas_ref></canvas> }
+    view! {
+        <canvas
+            _ref=canvas_ref
+            on:pointerdown=move |ev| {
+                let grid_x = ev.offset_x() as f64 / CELL_SIZE;
+                let grid_y = ev.offset_y() as f64 / CELL_SIZE;
+                let c = (grid_x as i32, grid_y as i32);
+                let cell = grid().get(&c).unwrap_or(&0).clone();
+                set_grid
+                    .update(|grid| {
+                        grid.insert(c, if cell == 0 { 1 } else { 0 });
+                    });
+            }
+        >
+        </canvas>
+    }
 }
